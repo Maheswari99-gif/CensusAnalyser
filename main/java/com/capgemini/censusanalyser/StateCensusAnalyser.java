215d28baac999fa3107d23db625eb44b39bf17f2
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-25 18:16:27 +0530

    MaheswariKoyyani : Ability to analyse IndianState census

---
+++ main/java/com/capgemini/censusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,32 @@
+package com.capgemini.censusanalyser;
+	import java.io.IOException;
+	import java.io.Reader;
+	import java.nio.file.Files;
+	import java.nio.file.Paths;
+	import java.util.Iterator;
+	import java.util.stream.StreamSupport;
+	import com.opencsv.bean.CsvToBean;
+	import com.opencsv.bean.CsvToBeanBuilder;
+
+	public class StateCensusAnalyser {
+		public int loadStateCensusData(String csvfilePath) throws CustomStateCensusAnalyserException {
+			try {	
+			Reader reader;
+				reader = Files.newBufferedReader(Paths.get(csvfilePath));
+				CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+				csvToBeanBuilder.withType(CSVStateCensus.class);
+				csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+				CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
+			    Iterator<CSVStateCensus> csvStateCensusIterator = csvToBean.iterator();
+				Iterable<CSVStateCensus> csvStateCensusIterable = () -> csvStateCensusIterator;
+				int numOfEntries = (int) StreamSupport.stream(csvStateCensusIterable.spliterator(), false).count();
+				return numOfEntries;
+			} catch (IOException e) {
+				throw new CustomStateCensusAnalyserException(ExceptionType.STATE_CENSUS_FILE_PROBLEM);
+			}
+			catch(IllegalStateException e) {
+				throw new CustomStateCensusAnalyserException(ExceptionType.STATE_CENSUS_PARSE_PROBLEM);
+			}
+		}
+	}
+
---
+++ test/java/com/capgemini/censusanalyser/CensusAnalyserTest.java
@@ -0,0 +1,30 @@
+package com.capgemini.censusanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+	private final String STATE_CENSUS_CSV_FILE = "./src/main/resources/StateCensusCSVData.csv";
+	private final String INCORRECT_STATE_CENSUS_CSV_FILE = "./src/main/resources/_StateCensusCSVData.csv";
+
+	@Test
+	public void givenTheStatesCensusCSVFile_WhenRead_NoOfRecordsShouldMatch()
+			throws CustomStateCensusAnalyserException {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		int numOfRecords = stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_CSV_FILE);
+		Assert.assertEquals(6, numOfRecords);
+	}
+
+	@Test
+	public void givenIncorrectCSVFile_ShouldReturnCustomException() {
+		String exceptionMessage = null;
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			stateCensusAnalyser.loadStateCensusData(INCORRECT_STATE_CENSUS_CSV_FILE);
+		} catch (CustomStateCensusAnalyserException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.STATE_CENSUS_FILE_PROBLEM.toString(), exceptionMessage);
+	}
+
+}
