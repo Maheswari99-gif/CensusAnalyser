51366f6fdc6677e06746c58335b3b1ed1b1579ae
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-25 23:43:25 +0530

    MaheswariKoyyani : Use Delegation Principle to handle CSV

--- main/java/com/capgemini/censusanalyser/StateCensusAnalyser.java
+++ main/java/com/capgemini/censusanalyser/StateCensusAnalyser.java
@@ -24,7 +24,7 @@ public class StateCensusAnalyser {
 		}
 	}
 
-	public int loadStateCodeData(String csvFilePath) throws CustomStateCensusAnalyserException {
+	/*public int loadStateCodeData(String csvFilePath) throws CustomStateCensusAnalyserException {
 		if (!(csvFilePath.matches(".*\\.csv$")))
 			throw new CustomStateCensusAnalyserException("Incorrect Type", ExceptionType.INCORRECT_TYPE);
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
@@ -34,7 +34,7 @@ public class StateCensusAnalyser {
 		} catch (IOException e) {
 			throw new CustomStateCensusAnalyserException("Incorrect CSV File", ExceptionType.STATE_CENSUS_FILE_PROBLEM);
 		}
-	}
+	}*/
 
 	public <T> Iterator<T> getIteratorFromCsv(Reader reader, Class<T> csvBindedClass)
 			throws CustomStateCensusAnalyserException {
