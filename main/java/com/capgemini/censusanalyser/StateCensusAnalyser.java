8cabb13d4c98b88220b8c2a637ce08471078a797
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-26 14:42:02 +0530

    MaheswariKoyyani : State Census Data in Json Format

--- main/java/com/capgemini/censusanalyser/StateCensusAnalyser.java
+++ main/java/com/capgemini/censusanalyser/StateCensusAnalyser.java
@@ -1,41 +1,71 @@
 package com.capgemini.censusanalyser;
 
 import com.capgemini.opencsvbuilder.*;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.StreamSupport;
 import com.opencsv.bean.MappingStrategy;
 
 public class StateCensusAnalyser {
-	public int loadStateCensusData(String csvFilePath, MappingStrategy<CSVStateCensus> mappingStrategy, Class<? extends CSVStateCensus> csvBinderClass, final char separator) throws CustomFileIOException, CustomCSVBuilderException {
-		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){	
-			List<CSVStateCensus> csvStateCensusList;
+
+	List<CSVStateCensus> csvStateCensusList;
+
+	public int loadStateCensusData(String csvFilePath, MappingStrategy<CSVStateCensus> mappingStrategy,
+			Class<? extends CSVStateCensus> csvBinderClass, final char separator)
+			throws CustomFileIOException, CustomCSVBuilderException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
 			ICsvBuilder csvBuilder = new CsvBuilderFactory().createCSVBuilder();
-			if(csvBinderClass != null)
-				csvStateCensusList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class, mappingStrategy, separator);
+			if (csvBinderClass != null)
+				csvStateCensusList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class, mappingStrategy,
+						separator);
 			else
 				csvStateCensusList = csvBuilder.getCSVFileList(reader, null, null, separator);
 			return csvStateCensusList.size();
 		} catch (IOException e) {
 			throw new CustomFileIOException(ExceptionTypeIO.FILE_PROBLEM);
-		} 
+		}
 	}
 
-	public int loadStateCodeData(String csvFilePath, MappingStrategy<CSVStates> mappingStrategy, Class<? extends CSVStates> csvBinderClass, final char separator) throws CustomFileIOException, CustomCSVBuilderException {
-		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){	
+	public int loadStateCodeData(String csvFilePath, MappingStrategy<CSVStates> mappingStrategy,
+			Class<? extends CSVStates> csvBinderClass, final char separator)
+			throws CustomFileIOException, CustomCSVBuilderException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
 			List<CSVStates> csvStateCodeList;
 			ICsvBuilder csvBuilder = new CsvBuilderFactory().createCSVBuilder();
-			if(csvBinderClass != null)
+			if (csvBinderClass != null)
 				csvStateCodeList = csvBuilder.getCSVFileList(reader, CSVStates.class, mappingStrategy, separator);
 			else
-				csvStateCodeList= csvBuilder.getCSVFileList(reader, null, null, separator);
+				csvStateCodeList = csvBuilder.getCSVFileList(reader, null, null, separator);
 			return csvStateCodeList.size();
 		} catch (IOException e) {
 			throw new CustomFileIOException(ExceptionTypeIO.FILE_PROBLEM);
-		} 
+		}
+	}
+
+	public String getAlpahebeticalStateWiseCensusData() {
+		Comparator<CSVStateCensus> censusComparator = Comparator.comparing(census -> census.state);
+		this.sort(censusComparator);
+		String sortedStateCensus = new Gson().toJson(csvStateCensusList);
+		return sortedStateCensus;
+	}
+
+	private void sort(Comparator<CSVStateCensus> censusComparator) {
+		for (int i = 0; i < csvStateCensusList.size(); i++) {
+			for (int j = 0; j < csvStateCensusList.size() - i - 1; j++) {
+				CSVStateCensus censusOne = csvStateCensusList.get(j);
+				CSVStateCensus censusTwo = csvStateCensusList.get(j + 1);
+				if (censusComparator.compare(censusOne, censusTwo) > 0) {
+					csvStateCensusList.set(j, censusTwo);
+					csvStateCensusList.set(j + 1, censusOne);
+				}
+			}
+		}
 	}
 }
